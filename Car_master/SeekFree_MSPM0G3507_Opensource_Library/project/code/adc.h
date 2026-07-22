fd18513b7cefd85d0fe40891e506a6f24a86d548
Fieem <2609940899@qq.com>
2026-07-22 19:19:56 +0800

    去除灰度中间部分

--- Car_master/SeekFree_MSPM0G3507_Opensource_Library/project/code/adc.h
+++ Car_master/SeekFree_MSPM0G3507_Opensource_Library/project/code/adc.h
@@ -65,7 +65,7 @@ typedef enum {
 #define TURN_SPEED_RIGHT         15.0f       // STATE_TURN右轮固定目标速度
 #define LINE_LOST_TURN_HOLD_MS  500U        // 连续丢线100ms后进入STATE_TURN
 #define STEER_POLARITY          (-1.0f)     // 转向极性：当前实车方向反向，统一翻转舵角和差速
-#define SPEED_DIFF_DEADZONE           1.5f  // 阿克曼差速死区，单位：目标速度
+#define SPEED_DIFF_DEADZONE           1.0f  // 阿克曼差速死区，单位：目标速度
 
 // 阿克曼几何参数：请按车上轮胎中心线实测值修改，单位 mm
 #define WHEEL_BASE_MM          120.0f       // 轴距：前后轮轴中心距离
